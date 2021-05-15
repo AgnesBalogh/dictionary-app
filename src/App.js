0937155c389cb7b046f8ef83b31581876043d994
AgnesBalogh <63673667+AgnesBalogh@users.noreply.github.com>
2021-05-15 17:24:31 +0300

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         
-      <h1>Dictionary App</h1>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer"><small>Open-source Code by Agnes Balogh</small></footer>
       </header>
+      </div>
     </div>
   );
 }
