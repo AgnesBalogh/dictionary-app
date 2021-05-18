9c430cdcc79638b943e685d082af2f87bf328554
AgnesBalogh <63673667+AgnesBalogh@users.noreply.github.com>
2021-05-18 12:00:05 +0300

    Added Synonyms.js

---
+++ src/Synonyms.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+export default function Synonyms(props) {
+    
+        if (props.synonyms) {
+                return (
+        <div>
+        <strong>Synonyms: </strong>
+         <ul>
+        {props.synonyms.map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>
+            
+        })}
+        </ul>;
+        </div>
+            );
+        } else {
+            return null;
+        }
+        
+    
+}
